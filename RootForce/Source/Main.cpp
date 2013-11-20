662c497d3bca614bece857a70e315d6171ab9a76
Daniel Carlsson <dc.jarle@hotmail.com>
2013-11-20 08:32:37 +0100

    Test

--- RootForce/Source/Main.cpp
+++ RootForce/Source/Main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
 Main::Main() 
 	: m_running(true) 
 {
-
+	int a = 0;
 	if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO) != 0) 
 	{
 		// TODO: Log error and throw exception (?)
