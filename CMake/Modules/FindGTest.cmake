52ffa61ea93e1bed630fb54ce287ede703b468b8
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2013-11-20 08:42:09 +0100

    Added GTest
    Also committing file glob source detection.

---
+++ CMake/Modules/FindGTest.cmake
@@ -0,0 +1,9 @@
+# Find the Google Test library
+
+find_path(GTEST_INCLUDE_DIRS
+    gtest/gtest.h
+    HINTS "${CMAKE_SOURCE_DIR}/External/Include")
+
+find_library(GTEST_LIBRARY
+    NAMES gtest
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib")
