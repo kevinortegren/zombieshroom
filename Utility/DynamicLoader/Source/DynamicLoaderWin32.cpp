a573719de649fd3cbdceb7b471851f3d7d10a394
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2013-11-21 13:48:05 +0100

    Added DynamicLoader library for loading DLLs
    Can be extended with a Unix implementation for future ports.

---
+++ Utility/DynamicLoader/Source/DynamicLoaderWin32.cpp
@@ -0,0 +1,26 @@
+#include <DynamicLoader.h>
+#include <Windows.h>
+
+/** Windows Implementation */
+namespace DynamicLoader
+{
+	void* LoadSharedLibrary(const char* filename)
+	{
+		return (void*) LoadLibraryA(filename);
+	}
+
+	void FreeSharedLibrary(void* library)
+	{
+		FreeLibrary((HMODULE)library);
+	}
+
+	void* LoadProcess(void* library, char* name)
+	{
+		return GetProcAddress((HMODULE)library, name);
+	}
+
+	const char* GetLastError()
+	{
+
+	}
+}
