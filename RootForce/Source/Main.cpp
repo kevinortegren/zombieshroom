415af36a87f096953689394b79bd97b78c4ede44
kevinortegren <kevinortegren@gmail.com>
2013-11-26 14:06:41 +0100

    Removed old comments

--- RootForce/Source/Main.cpp
+++ RootForce/Source/Main.cpp
@@ -106,17 +106,6 @@ Main::~Main()
 
 void Main::Start() 
 {
-
-	//Open the log file stream for this instance(Do this once at the beginning of the program)
-	//Logging::GetInstance()->OpenLogStream();
-
-	//Include Logging.h in the file you want to use the logging function
-	//Write a string to the log file stream(Do this when you want to log something...)
-	//Logging::GetInstance()->LogTextToFile("Log %f entry test ", 1.435);
-	
-	//Write a log string to console
-	//Logging::GetInstance()->LogTextToConsole("Console entry test %d", 12);
-
 	int numVertices = 3;
 	Render::Vertex1P1N realVertices[3];
 	realVertices[0].m_pos = glm::vec3(0.5f, 0.5f, 1.f); realVertices[0].m_normal = glm::vec3(0.f, 0.f, 1.f);
