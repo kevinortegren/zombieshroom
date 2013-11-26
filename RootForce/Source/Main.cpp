ce216d913661ecfdcb48ecbcbed9b6f7436caebb
kevinortegren <kevinortegren@gmail.com>
2013-11-26 15:02:16 +0100

    nop

--- RootForce/Source/Main.cpp
+++ RootForce/Source/Main.cpp
@@ -128,7 +128,6 @@ void Main::Start()
 	job.m_mesh = m_engineContext.m_resourceManager->GetModel("testchar")->m_meshes[0];
 	job.m_uniforms = &uniforms;
 	job.m_effect = m_engineContext.m_resourceManager->GetEffect("test");
-	
 	float angle = 0.0f;
 
 	uint64_t old = SDL_GetPerformanceCounter();
