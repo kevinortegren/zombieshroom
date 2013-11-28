088d41936f8b06f4a1c41c54fce5198660ff5bca
Gustav Pihl <phlogios@gmail.com>
2013-11-28 15:18:18 +0100

    Oops worked in Master, didn't break anything

--- RootEngine/Include/SubsystemSharedContext.h
+++ RootEngine/Include/SubsystemSharedContext.h
@@ -2,6 +2,7 @@
 
 #include <RootEngine/Include/Logging/Logging.h>
 #include <RootEngine/Include/Memory/MemoryTracker.h>
+//#include <RootEngine/Include/ResourceManager/ResourceManager.h>
 
 namespace RootEngine
 {
@@ -9,6 +10,7 @@ namespace RootEngine
 	{
 		Logging* m_logger;
 		MemoryTracker* m_memTracker;
+		//ResourceManagerInterface* m_resourceManager;
 	};
 
 	/** This class needs to be implemented by all subsystem interfaces */
