0b699312c62efb6f401b970f028356bf56982e38
Thomas Sievert <thomas.t.m.sievert@gmail.com>
2013-12-04 09:05:40 +0100

    WIP

--- RootForce/Include/ServerSystem.h
+++ RootForce/Include/ServerSystem.h
@@ -7,6 +7,9 @@ namespace RootForce
 	class ServerSystem : public ECS::EntitySystem
 	{
 	public:
+		ServerSystem(uint16_t p_serverPort);
+		ServerSystem(const char* p_address, uint16_t p_port);
+
 		void Init();
 		void Begin();
 		void ProcessEntity(ECS::Entity* p_entity);
---
+++ RootForce/Source/ServerSystem.cpp
@@ -0,0 +1,25 @@
+#include <ServerSystem.h>
+
+namespace RootForce
+{
+	void ServerSystem::Init()
+	{
+
+	}
+
+	void ServerSystem::Begin()
+	{
+
+	}
+
+	void ServerSystem::ProcessEntity(ECS::Entity* p_entity)
+	{
+
+	}
+
+	void ServerSystem::End()
+	{
+
+	}
+
+}
