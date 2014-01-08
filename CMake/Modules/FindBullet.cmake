4ba02d3d7a4c173a290543e42d53691cd44841b8
Erik <erik_the_great_one@hotmail.com>
2014-01-08 09:59:03 +0100

    Fixed 32/64 release/debug libs and added option in cmake to build release version

--- CMake/Modules/FindBullet.cmake
+++ CMake/Modules/FindBullet.cmake
@@ -4,12 +4,32 @@ find_path(BULLET_INCLUDE_DIRS
     Bullet/btBulletCollisionCommon.h
     HINTS "${CMAKE_SOURCE_DIR}/External/Include")
 
-find_library(BULLETCOLLISION_LIBRARY
+find_library(BULLETCOLLISION_LIBRARY_DEBUG
     NAMES BulletCollision_vs2010_debug
-    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86")
-find_library(BULLETDYNAMICS_LIBRARY
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Debug")
+find_library(BULLETDYNAMICS_LIBRARY_DEBUG
     NAMES BulletDynamics_vs2010_debug
-    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86")
-find_library(LINEARMATH_LIBRARY
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Debug")
+find_library(LINEARMATH_LIBRARY_DEBUG
     NAMES LinearMath_vs2010_debug
-    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86")
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Debug")
+	
+find_library(BULLETCOLLISION_LIBRARY
+    NAMES BulletCollision_vs2010
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Release")
+find_library(BULLETDYNAMICS_LIBRARY
+    NAMES BulletDynamics_vs2010
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Release")
+find_library(LINEARMATH_LIBRARY
+    NAMES LinearMath_vs2010
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Release")
+	
+find_library(BULLETCOLLISION_LIBRARY_DEBUG_64
+    NAMES BulletCollision_vs2010_x64_debug
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x64/Debug")
+find_library(BULLETDYNAMICS_LIBRARY_DEBUG_64
+    NAMES BulletDynamics_vs2010_x64_debug
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x64/Debug")
+find_library(LINEARMATH_LIBRARY_DEBUG_64
+    NAMES LinearMath_vs2010_x64_debug
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x64/Debug")
--- CMake/Modules/FindRakNet.cmake
+++ CMake/Modules/FindRakNet.cmake
@@ -6,4 +6,14 @@ find_path(RAKNET_INCLUDE_DIRS
 
 find_library(RAKNET_LIBRARY
     NAMES RakNet_VS2008_DLL_Debug_Win32
-    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86")
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Debug")
+find_library(RAKNET_LIBRARY
+    NAMES RakNet_VS2008_DLL_Release_Win32
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Release")
+	
+find_library(RAKNET_LIBRARY
+    NAMES RakNet_VS2008_DLL_Debug_x64
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x64/Debug")
+find_library(RAKNET_LIBRARY
+    NAMES RakNet_VS2008_DLL_Release_x64
+    HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x64/Release")
