a93f0b04306fa156842c0fe71bbfe3b97e0d25f0
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2014-01-15 10:56:33 +0100

    Slimmed current scripts

--- Assets/Scripts/AbilityTest.lua
+++ Assets/Scripts/AbilityTest.lua
@@ -4,27 +4,14 @@ ACTION_COLLIDE = 1;
 --Set table name
 AbilityTest = {};
 
-function AbilityTest.Process (action, ...)
-	
-	local args = table.pack(...);
-
-	if action == ACTION_CREATE then
-		AbilityTest.OnCreate();
-	elseif action == ACTION_COLLIDE then
-		AbilityTest.OnCollide(args);
-	end
-	
-end
-
 function AbilityTest.OnCreate ()
-		local playerEnt = Entity.GetEntityByTag("Player");
-		local frontVec  	= Entity.GetEntityByTag("AimingDevice"):GetTransformation():GetOrient():GetFront();
-		local handle = playerEnt:GetCollision():GetHandle();
-		playerEnt:GetPhysics():KnockBack(handle, Vec3.New(0,0.1,0)+frontVec, 100);
-	
+	local playerEnt = Entity.GetEntityByTag("Player");
+	local frontVec  	= Entity.GetEntityByTag("AimingDevice"):GetTransformation():GetOrient():GetFront();
+	local handle = playerEnt:GetCollision():GetHandle();
+	playerEnt:GetPhysics():KnockBack(handle, Vec3.New(0,0.1,0)+frontVec, 100);
 end
 
-function AbilityTest.OnCollide (args)
+function AbilityTest.OnCollide (entityId)
 	
 end
 
