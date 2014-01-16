0a31e454f39d669d21063e7ab996ec4a9fbae056
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2014-01-16 12:01:06 +0100

    WIP - Working on serializing/deserializing entities.

--- RootSystems/Include/Network/NetworkEntityMap.h
+++ RootSystems/Include/Network/NetworkEntityMap.h
@@ -1,50 +1,21 @@
 #pragma once
 
-#include <map>
 #include <stdint.h>
 #include <RakNet/RakPeerInterface.h>
-#include <Utility/ECS/Include/EntitySystem.h>
+#include <RakNet/RakString.h>
+#include <RootSystems/Include/Components.h>
+#include <Utility/ECS/Include/Entity.h>
+#include <Utility/ECS/Include/Component.h>
 
 namespace RootForce
 {
 	namespace Network
 	{
-		typedef int16_t TemporaryId_t;
-		typedef int16_t SynchronizedId_t;
+		typedef uint16_t UserID_t;
+		typedef uint32_t ActionID_t;
+		typedef uint16_t SequenceID_t;
+		typedef uint64_t SynchronizedID_t;
 
-		const TemporaryId_t TEMPORARY_ID_NONE = -1;
-		const SynchronizedId_t SYNCHRONIZED_ID_NONE = -1;
-
-		class NetworkEntityMap
-		{
-		public:
-			NetworkEntityMap();
-
-			TemporaryId_t AddEntity(ECS::Entity* p_entity);
-			void SetSynchronizedId(TemporaryId_t p_temporaryId, SynchronizedId_t p_synchronizedId);
-			void RemoveSynchronizedEntity(SynchronizedId_t p_synchronizedId);
-
-			SynchronizedId_t GetSynchronizedId(ECS::Entity* p_entity) const;
-			TemporaryId_t GetTemporaryId(ECS::Entity* p_entity) const;
-			ECS::Entity* GetSynchronizedEntity(SynchronizedId_t p_synchronizedId) const;
-			ECS::Entity* GetTemporaryEntity(TemporaryId_t p_temporaryId) const;
-
-			void AssociatePlayerEntityWithUserID(int8_t p_userId, ECS::Entity* p_entity);
-			ECS::Entity* GetPlayerEntityFromUserID(int8_t p_userId) const;
-
-			/** Only used by server */
-			SynchronizedId_t NextSynchronizedId();
-		private:
-			std::map<TemporaryId_t, ECS::Entity*> m_temporaryEntityMap;
-			std::map<SynchronizedId_t, ECS::Entity*> m_synchronizedEntityMap;
-			std::map<int8_t, ECS::Entity*> m_playerEntityMap;
-
-			TemporaryId_t m_nextTemporaryId;
-			SynchronizedId_t m_nextSynchronizedId;
-		};
-
-
-		
 		/** Specifies reserved user IDs. Connected peers will start counting from index 0. */
 		namespace ReservedUserID
 		{
@@ -65,7 +36,7 @@ namespace RootForce
 			};
 		}
 
-		/** 
+		/*
 			Uniquely identifies an entity across all peers.
 
 			UserID - Identifies the peer, creator of the entity.
@@ -78,20 +49,23 @@ namespace RootForce
 		{
 			struct
 			{
-				uint16_t UserID;
-				uint32_t ActionID;
-				uint16_t SequenceID;
+				UserID_t UserID;
+				ActionID_t ActionID;
+				SequenceID_t SequenceID;
 			};
 
-			uint64_t SynchronizedID;
+			SynchronizedID_t SynchronizedID;
 		};
 
-		/** Add this component onto every entity that you want to synchronize over the network. */
+
+		/* 
+			Add this component onto every entity that you want to synchronize over the network. 
+		*/
 		struct NetworkComponent
 		{
-			static int16_t sUserID;
-			static int32_t sCurrentActionID;
-			static int16_t sNextSequenceID;
+			static UserID_t sUserID;
+			static ActionID_t sCurrentActionID;
+			static SequenceID_t sNextSequenceID;
 			
 			NetworkEntityID ID;
 			
@@ -101,6 +75,71 @@ namespace RootForce
 				ID.ActionID = sCurrentActionID;
 				ID.SequenceID = sNextSequenceID++;
 			}
+		};
+
+
+
+		/*
+			Used to serialize/deserialize a component.
+		*/
+		struct SerializableComponent
+		{
+			ComponentType::ComponentType Type;
+			ECS::ComponentInterface* Component;
+
+			void Serialize(bool p_write, RakNet::BitStream* p_bs);
+		};
+
+		struct SerializableEntity
+		{
+			ECS::Entity* Entity;
+
+			void Serialize(bool p_write, RakNet::BitStream* p_bs);
+		};
+		
+		
+
+		/*
+			Used to serialize/deserialize an entity.
+		*/
+		void Serialize(bool p_write, RakNet::BitStream* p_bs, ECS::Entity* p_entity, NetworkEntityID p_id);
+
+
+
+
+		/*
+			Defines a serialized component that can be sent across the network
+		*/
+		struct SerializedComponent
+		{
+			ComponentType::ComponentType Type;
+			unsigned int DataSize;
+			unsigned char* Data;
+
+			void Serialize(bool p_write, RakNet::BitStream* p_bs);
+			void SerializeComponent(ECS::ComponentInterface* p_component, ComponentType::ComponentType p_type);
+			void DeserializeComponent(ECS::ComponentInterface* p_component);
+		};
+
+		/*
+			Defines a serialized entity that can be sent across the network
+		*/
+		struct SerializedEntity
+		{
+			NetworkEntityID ID;
+			RakNet::RakString Script;
+			std::vector<SerializedComponent> Components;
+
+			void Serialize(bool p_writeToBitstream, RakNet::BitStream* p_bs);
+			void SerializeEntity(ECS::Entity* p_entity);
+		};
+
+		/*
+			Sent occasionally to clients to synchronize entities.
+		*/
+		struct MessageDeltaGameState
+		{
+			std::vector<SerializedEntity> Entities;
 		};
 	}
 }
--- RootSystems/Source/Network/NetworkEntityMap.cpp
+++ RootSystems/Source/Network/NetworkEntityMap.cpp
@@ -1,89 +1,18 @@
 #include <Network/NetworkEntityMap.h>
-#include <Utility/ECS/Include/World.h>
+#include <RakNet/BitStream.h>
 
 namespace RootForce
 {
 	namespace Network
 	{
-		NetworkEntityMap::NetworkEntityMap()
-			: m_nextSynchronizedId(0)
-			, m_nextTemporaryId(0)
-		{}
-
-
-		TemporaryId_t NetworkEntityMap::AddEntity(ECS::Entity* p_entity)
+		void Serialize(bool p_write, RakNet::BitStream* p_bs, ECS::ComponentInterface* p_component, ComponentType::ComponentType p_type)
 		{
-			m_temporaryEntityMap[m_nextTemporaryId] = p_entity;
-			return m_nextTemporaryId++;
+			
 		}
 
-		void NetworkEntityMap::SetSynchronizedId(TemporaryId_t p_temporaryId, SynchronizedId_t p_synchronizedId)
+		void Serialize(bool p_write, RakNet::BitStream* p_bs, ECS::Entity* p_entity, NetworkEntityID p_id)
 		{
-			m_synchronizedEntityMap[p_synchronizedId] = m_temporaryEntityMap[p_temporaryId];
-			m_temporaryEntityMap.erase(m_temporaryEntityMap.find(p_temporaryId));
-		}
 
-		void NetworkEntityMap::RemoveSynchronizedEntity(SynchronizedId_t p_synchronizedId)
-		{
-			m_synchronizedEntityMap.erase(p_synchronizedId);
-		}
-
-		SynchronizedId_t NetworkEntityMap::GetSynchronizedId(ECS::Entity* p_entity) const
-		{
-			for (auto it = m_synchronizedEntityMap.begin(); it != m_synchronizedEntityMap.end(); it++)
-			{
-				if (it->second == p_entity)
-					return it->first;
-			}
-
-			return SYNCHRONIZED_ID_NONE;
-		}
-
-		TemporaryId_t NetworkEntityMap::GetTemporaryId(ECS::Entity* p_entity) const
-		{
-			for (auto it = m_temporaryEntityMap.begin(); it != m_temporaryEntityMap.end(); it++)
-			{
-				if (it->second == p_entity)
-					return it->first;
-			}
-
-			return TEMPORARY_ID_NONE;
-		}
-
-		ECS::Entity* NetworkEntityMap::GetSynchronizedEntity(SynchronizedId_t p_synchronizedId) const
-		{
-			auto it = m_synchronizedEntityMap.find(p_synchronizedId);
-			if (it == m_synchronizedEntityMap.end())
-				return nullptr;
-			else
-				return it->second;
-		}
-
-		ECS::Entity* NetworkEntityMap::GetTemporaryEntity(TemporaryId_t p_temporaryId) const
-		{
-			auto it = m_temporaryEntityMap.find(p_temporaryId);
-			if (it == m_temporaryEntityMap.end())
-				return nullptr;
-			else
-				return it->second;
-		}
-
-		void NetworkEntityMap::AssociatePlayerEntityWithUserID(int8_t p_userId, ECS::Entity* p_entity)
-		{
-			m_playerEntityMap[p_userId] = p_entity;
-		}
-
-		ECS::Entity* NetworkEntityMap::GetPlayerEntityFromUserID(int8_t p_userId) const
-		{
-			auto it = m_playerEntityMap.find(p_userId);
-			if (it == m_playerEntityMap.end())
-				return nullptr;
-			return it->second;
-		}
-
-		SynchronizedId_t NetworkEntityMap::NextSynchronizedId()
-		{
-			return m_nextSynchronizedId++;
 		}
 	}
 }
