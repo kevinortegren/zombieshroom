1f5cc508e0bbe324802104e506502387ce491fac
kevinortegren <kevinortegren@gmail.com>
2014-01-23 19:44:03 +0100

    Fool proofing the particle editor

--- RootTools/ParticleEditor/GeneratedFiles/moc_ParticleEditor.cpp
+++ RootTools/ParticleEditor/GeneratedFiles/moc_ParticleEditor.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_ParticleEditor_t {
-    QByteArrayData data[47];
-    char stringdata[686];
+    QByteArrayData data[48];
+    char stringdata[699];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,63 +38,65 @@ QT_MOC_LITERAL(5, 42, 10),
 QT_MOC_LITERAL(6, 53, 8),
 QT_MOC_LITERAL(7, 62, 13),
 QT_MOC_LITERAL(8, 76, 13),
-QT_MOC_LITERAL(9, 90, 21),
-QT_MOC_LITERAL(10, 112, 24),
-QT_MOC_LITERAL(11, 137, 10),
-QT_MOC_LITERAL(12, 148, 13),
-QT_MOC_LITERAL(13, 162, 13),
-QT_MOC_LITERAL(14, 176, 15),
-QT_MOC_LITERAL(15, 192, 16),
-QT_MOC_LITERAL(16, 209, 16),
-QT_MOC_LITERAL(17, 226, 3),
-QT_MOC_LITERAL(18, 230, 16),
-QT_MOC_LITERAL(19, 247, 3),
-QT_MOC_LITERAL(20, 251, 16),
-QT_MOC_LITERAL(21, 268, 3),
-QT_MOC_LITERAL(22, 272, 15),
-QT_MOC_LITERAL(23, 288, 5),
-QT_MOC_LITERAL(24, 294, 15),
-QT_MOC_LITERAL(25, 310, 15),
-QT_MOC_LITERAL(26, 326, 18),
-QT_MOC_LITERAL(27, 345, 18),
-QT_MOC_LITERAL(28, 364, 15),
-QT_MOC_LITERAL(29, 380, 15),
-QT_MOC_LITERAL(30, 396, 15),
-QT_MOC_LITERAL(31, 412, 17),
-QT_MOC_LITERAL(32, 430, 17),
-QT_MOC_LITERAL(33, 448, 17),
-QT_MOC_LITERAL(34, 466, 15),
-QT_MOC_LITERAL(35, 482, 15),
-QT_MOC_LITERAL(36, 498, 13),
-QT_MOC_LITERAL(37, 512, 16),
-QT_MOC_LITERAL(38, 529, 12),
-QT_MOC_LITERAL(39, 542, 15),
-QT_MOC_LITERAL(40, 558, 20),
-QT_MOC_LITERAL(41, 579, 7),
-QT_MOC_LITERAL(42, 587, 11),
-QT_MOC_LITERAL(43, 599, 15),
-QT_MOC_LITERAL(44, 615, 18),
-QT_MOC_LITERAL(45, 634, 23),
-QT_MOC_LITERAL(46, 658, 26)
+QT_MOC_LITERAL(9, 90, 12),
+QT_MOC_LITERAL(10, 103, 21),
+QT_MOC_LITERAL(11, 125, 24),
+QT_MOC_LITERAL(12, 150, 10),
+QT_MOC_LITERAL(13, 161, 13),
+QT_MOC_LITERAL(14, 175, 13),
+QT_MOC_LITERAL(15, 189, 15),
+QT_MOC_LITERAL(16, 205, 16),
+QT_MOC_LITERAL(17, 222, 16),
+QT_MOC_LITERAL(18, 239, 3),
+QT_MOC_LITERAL(19, 243, 16),
+QT_MOC_LITERAL(20, 260, 3),
+QT_MOC_LITERAL(21, 264, 16),
+QT_MOC_LITERAL(22, 281, 3),
+QT_MOC_LITERAL(23, 285, 15),
+QT_MOC_LITERAL(24, 301, 5),
+QT_MOC_LITERAL(25, 307, 15),
+QT_MOC_LITERAL(26, 323, 15),
+QT_MOC_LITERAL(27, 339, 18),
+QT_MOC_LITERAL(28, 358, 18),
+QT_MOC_LITERAL(29, 377, 15),
+QT_MOC_LITERAL(30, 393, 15),
+QT_MOC_LITERAL(31, 409, 15),
+QT_MOC_LITERAL(32, 425, 17),
+QT_MOC_LITERAL(33, 443, 17),
+QT_MOC_LITERAL(34, 461, 17),
+QT_MOC_LITERAL(35, 479, 15),
+QT_MOC_LITERAL(36, 495, 15),
+QT_MOC_LITERAL(37, 511, 13),
+QT_MOC_LITERAL(38, 525, 16),
+QT_MOC_LITERAL(39, 542, 12),
+QT_MOC_LITERAL(40, 555, 15),
+QT_MOC_LITERAL(41, 571, 20),
+QT_MOC_LITERAL(42, 592, 7),
+QT_MOC_LITERAL(43, 600, 11),
+QT_MOC_LITERAL(44, 612, 15),
+QT_MOC_LITERAL(45, 628, 18),
+QT_MOC_LITERAL(46, 647, 23),
+QT_MOC_LITERAL(47, 671, 26)
     },
     "ParticleEditor\0MenuNew\0\0MenuOpen\0"
     "MenuSave\0ManuSaveAs\0MenuExit\0MenuViewStats\0"
-    "MenuHelpAbout\0MenuViewColorTriangle\0"
-    "MenuViewColorEndTriangle\0NewEmitter\0"
-    "DeleteEmitter\0RenameEmitter\0EmitterSelected\0"
-    "QListWidgetItem*\0PositionXChanged\0p_x\0"
-    "PositionYChanged\0p_y\0PositionZChanged\0"
-    "p_z\0SizeMinXChanged\0p_val\0SizeMaxXChanged\0"
-    "SizeEndXChanged\0LifeTimeMinChanged\0"
-    "LifeTimeMaxChanged\0GravityXChanged\0"
-    "GravityYChanged\0GravityZChanged\0"
-    "DirectionXChanged\0DirectionYChanged\0"
-    "DirectionZChanged\0SpeedMinChanged\0"
-    "SpeedMaxChanged\0SpreadChanged\0"
-    "SpawnTimeChanged\0ColorChanged\0"
-    "ColorEndChanged\0TextureDoubleClicked\0"
-    "p_index\0GridToggled\0GridSizeChanged\0"
-    "FocusButtonClicked\0colorAlphaSliderChanged\0"
+    "MenuHelpAbout\0MenuHelpHelp\0"
+    "MenuViewColorTriangle\0MenuViewColorEndTriangle\0"
+    "NewEmitter\0DeleteEmitter\0RenameEmitter\0"
+    "EmitterSelected\0QListWidgetItem*\0"
+    "PositionXChanged\0p_x\0PositionYChanged\0"
+    "p_y\0PositionZChanged\0p_z\0SizeMinXChanged\0"
+    "p_val\0SizeMaxXChanged\0SizeEndXChanged\0"
+    "LifeTimeMinChanged\0LifeTimeMaxChanged\0"
+    "GravityXChanged\0GravityYChanged\0"
+    "GravityZChanged\0DirectionXChanged\0"
+    "DirectionYChanged\0DirectionZChanged\0"
+    "SpeedMinChanged\0SpeedMaxChanged\0"
+    "SpreadChanged\0SpawnTimeChanged\0"
+    "ColorChanged\0ColorEndChanged\0"
+    "TextureDoubleClicked\0p_index\0GridToggled\0"
+    "GridSizeChanged\0FocusButtonClicked\0"
+    "colorAlphaSliderChanged\0"
     "endColorAlphaSliderChanged\0"
 };
 #undef QT_MOC_LITERAL
@@ -105,7 +107,7 @@ static const uint qt_meta_data_ParticleEditor[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      39,   14, // methods
+      40,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -113,45 +115,46 @@ static const uint qt_meta_data_ParticleEditor[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  209,    2, 0x08,
-       3,    0,  210,    2, 0x08,
-       4,    0,  211,    2, 0x08,
-       5,    0,  212,    2, 0x08,
-       6,    0,  213,    2, 0x08,
-       7,    0,  214,    2, 0x08,
-       8,    0,  215,    2, 0x08,
-       9,    0,  216,    2, 0x08,
-      10,    0,  217,    2, 0x08,
-      11,    0,  218,    2, 0x08,
-      12,    0,  219,    2, 0x08,
-      13,    0,  220,    2, 0x08,
-      14,    1,  221,    2, 0x08,
-      16,    1,  224,    2, 0x08,
-      18,    1,  227,    2, 0x08,
-      20,    1,  230,    2, 0x08,
-      22,    1,  233,    2, 0x08,
-      24,    1,  236,    2, 0x08,
-      25,    1,  239,    2, 0x08,
-      26,    1,  242,    2, 0x08,
-      27,    1,  245,    2, 0x08,
-      28,    1,  248,    2, 0x08,
-      29,    1,  251,    2, 0x08,
-      30,    1,  254,    2, 0x08,
-      31,    1,  257,    2, 0x08,
-      32,    1,  260,    2, 0x08,
-      33,    1,  263,    2, 0x08,
-      34,    1,  266,    2, 0x08,
-      35,    1,  269,    2, 0x08,
-      36,    1,  272,    2, 0x08,
-      37,    1,  275,    2, 0x08,
-      38,    1,  278,    2, 0x08,
-      39,    1,  281,    2, 0x08,
-      40,    1,  284,    2, 0x08,
-      42,    0,  287,    2, 0x08,
-      43,    1,  288,    2, 0x08,
-      44,    0,  291,    2, 0x08,
-      45,    1,  292,    2, 0x08,
-      46,    1,  295,    2, 0x08,
+       1,    0,  214,    2, 0x08,
+       3,    0,  215,    2, 0x08,
+       4,    0,  216,    2, 0x08,
+       5,    0,  217,    2, 0x08,
+       6,    0,  218,    2, 0x08,
+       7,    0,  219,    2, 0x08,
+       8,    0,  220,    2, 0x08,
+       9,    0,  221,    2, 0x08,
+      10,    0,  222,    2, 0x08,
+      11,    0,  223,    2, 0x08,
+      12,    0,  224,    2, 0x08,
+      13,    0,  225,    2, 0x08,
+      14,    0,  226,    2, 0x08,
+      15,    1,  227,    2, 0x08,
+      17,    1,  230,    2, 0x08,
+      19,    1,  233,    2, 0x08,
+      21,    1,  236,    2, 0x08,
+      23,    1,  239,    2, 0x08,
+      25,    1,  242,    2, 0x08,
+      26,    1,  245,    2, 0x08,
+      27,    1,  248,    2, 0x08,
+      28,    1,  251,    2, 0x08,
+      29,    1,  254,    2, 0x08,
+      30,    1,  257,    2, 0x08,
+      31,    1,  260,    2, 0x08,
+      32,    1,  263,    2, 0x08,
+      33,    1,  266,    2, 0x08,
+      34,    1,  269,    2, 0x08,
+      35,    1,  272,    2, 0x08,
+      36,    1,  275,    2, 0x08,
+      37,    1,  278,    2, 0x08,
+      38,    1,  281,    2, 0x08,
+      39,    1,  284,    2, 0x08,
+      40,    1,  287,    2, 0x08,
+      41,    1,  290,    2, 0x08,
+      43,    0,  293,    2, 0x08,
+      44,    1,  294,    2, 0x08,
+      45,    0,  297,    2, 0x08,
+      46,    1,  298,    2, 0x08,
+      47,    1,  301,    2, 0x08,
 
  // slots: parameters
     QMetaType::Void,
@@ -166,33 +169,34 @@ static const uint qt_meta_data_ParticleEditor[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 15,    2,
-    QMetaType::Void, QMetaType::Double,   17,
-    QMetaType::Void, QMetaType::Double,   19,
-    QMetaType::Void, QMetaType::Double,   21,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::Double,   23,
-    QMetaType::Void, QMetaType::QColor,   23,
-    QMetaType::Void, QMetaType::QColor,   23,
-    QMetaType::Void, QMetaType::QModelIndex,   41,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Double,   23,
+    QMetaType::Void, 0x80000000 | 16,    2,
+    QMetaType::Void, QMetaType::Double,   18,
+    QMetaType::Void, QMetaType::Double,   20,
+    QMetaType::Void, QMetaType::Double,   22,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::QColor,   24,
+    QMetaType::Void, QMetaType::QColor,   24,
+    QMetaType::Void, QMetaType::QModelIndex,   42,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   23,
-    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   24,
+    QMetaType::Void, QMetaType::Int,   24,
 
        0        // eod
 };
@@ -209,38 +213,39 @@ void ParticleEditor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
         case 4: _t->MenuExit(); break;
         case 5: _t->MenuViewStats(); break;
         case 6: _t->MenuHelpAbout(); break;
-        case 7: _t->MenuViewColorTriangle(); break;
-        case 8: _t->MenuViewColorEndTriangle(); break;
-        case 9: _t->NewEmitter(); break;
-        case 10: _t->DeleteEmitter(); break;
-        case 11: _t->RenameEmitter(); break;
-        case 12: _t->EmitterSelected((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
-        case 13: _t->PositionXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 14: _t->PositionYChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 15: _t->PositionZChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 16: _t->SizeMinXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 17: _t->SizeMaxXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 18: _t->SizeEndXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 19: _t->LifeTimeMinChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 20: _t->LifeTimeMaxChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 21: _t->GravityXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 22: _t->GravityYChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 23: _t->GravityZChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 24: _t->DirectionXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 25: _t->DirectionYChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 26: _t->DirectionZChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 27: _t->SpeedMinChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 28: _t->SpeedMaxChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 29: _t->SpreadChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 30: _t->SpawnTimeChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 31: _t->ColorChanged((*reinterpret_cast< const QColor(*)>(_a[1]))); break;
-        case 32: _t->ColorEndChanged((*reinterpret_cast< const QColor(*)>(_a[1]))); break;
-        case 33: _t->TextureDoubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 34: _t->GridToggled(); break;
-        case 35: _t->GridSizeChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
-        case 36: _t->FocusButtonClicked(); break;
-        case 37: _t->colorAlphaSliderChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 38: _t->endColorAlphaSliderChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->MenuHelpHelp(); break;
+        case 8: _t->MenuViewColorTriangle(); break;
+        case 9: _t->MenuViewColorEndTriangle(); break;
+        case 10: _t->NewEmitter(); break;
+        case 11: _t->DeleteEmitter(); break;
+        case 12: _t->RenameEmitter(); break;
+        case 13: _t->EmitterSelected((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 14: _t->PositionXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 15: _t->PositionYChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 16: _t->PositionZChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 17: _t->SizeMinXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 18: _t->SizeMaxXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 19: _t->SizeEndXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 20: _t->LifeTimeMinChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 21: _t->LifeTimeMaxChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 22: _t->GravityXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 23: _t->GravityYChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 24: _t->GravityZChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 25: _t->DirectionXChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 26: _t->DirectionYChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 27: _t->DirectionZChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 28: _t->SpeedMinChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 29: _t->SpeedMaxChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 30: _t->SpreadChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 31: _t->SpawnTimeChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 32: _t->ColorChanged((*reinterpret_cast< const QColor(*)>(_a[1]))); break;
+        case 33: _t->ColorEndChanged((*reinterpret_cast< const QColor(*)>(_a[1]))); break;
+        case 34: _t->TextureDoubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 35: _t->GridToggled(); break;
+        case 36: _t->GridSizeChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 37: _t->FocusButtonClicked(); break;
+        case 38: _t->colorAlphaSliderChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 39: _t->endColorAlphaSliderChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -271,13 +276,13 @@ int ParticleEditor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 39)
+        if (_id < 40)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 39;
+        _id -= 40;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 39)
+        if (_id < 40)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 39;
+        _id -= 40;
     }
     return _id;
 }
