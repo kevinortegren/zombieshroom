533c29d5d016263820582fbb44bcd40c61a002d7
doqunbop <deadlyak@gmail.com>
2014-01-29 17:06:35 +0100

    Unit test in progress.

---
+++ RootTools/UnitTesting/Source/TDMRuleSet.cpp
@@ -0,0 +1,68 @@
+#include "UnitTesting.h"
+#include <RootEngine/Script/Include/RootScript.h>
+
+TEST(TDMRuleSet, AwardPlayerKill) 
+{
+	ECS::World* world = CreateWorld();
+	g_world = world;
+	g_networkEntityMap.clear();
+
+	// Call the OnCreate script
+	g_networkEntityMap.clear();
+	RootForce::Network::NetworkComponent::s_sequenceIDMap.clear();
+	g_engineContext.m_script->SetFunction(g_engineContext.m_resourceManager->LoadScript("MatchState"), "OnCreate");
+	g_engineContext.m_script->AddParameterNumber(RootForce::Network::ReservedUserID::NONE);
+	g_engineContext.m_script->AddParameterNumber(RootForce::Network::ReservedActionID::NONE);
+	g_engineContext.m_script->ExecuteScript();
+
+	// Create a player entity
+	g_engineContext.m_script->SetGlobalNumber("UserID", 0);
+	g_engineContext.m_script->SetGlobalNumber("IsClient", false);
+	g_engineContext.m_script->SetFunction(g_engineContext.m_resourceManager->LoadScript("Player"), "OnCreate");
+	g_engineContext.m_script->AddParameterNumber(0);
+	g_engineContext.m_script->AddParameterNumber(RootForce::Network::ReservedActionID::CONNECT);
+	g_engineContext.m_script->ExecuteScript();
+
+	// Get the newly created player
+	ECS::Entity* player = world->GetTagManager()->GetEntityByTag("Player");
+	RootForce::PlayerComponent* playerComponent = g_world->GetEntityManager()->GetComponent<RootForce::PlayerComponent>(player);
+
+	{
+		EXPECT_EQ(playerComponent->Deaths, 0);
+
+		// Force the player to commit a suicide
+		RootForce::MatchStateSystem::AwardPlayerKill(RootForce::Network::ReservedUserID::NONE, 0); // 0 is the ID of the test player
+
+		EXPECT_EQ(playerComponent->Deaths, 1);
+	}
+
+	{
+		// Create another player entity
+		g_engineContext.m_script->SetFunction(g_engineContext.m_resourceManager->LoadScript("Player"), "OnCreate");
+		g_engineContext.m_script->AddParameterNumber(1);
+		g_engineContext.m_script->AddParameterNumber(RootForce::Network::ReservedActionID::CONNECT);
+		g_engineContext.m_script->ExecuteScript();
+
+		// Make the second player kill the first one, yay!
+		RootForce::MatchStateSystem::AwardPlayerKill(1, 0); // 1 is the ID of the 2nd player, 0 is the ID of the 1st.
+
+		RootForce::PlayerComponent* secondPlayerComponent = g_world->GetEntityManager()->GetComponent<RootForce::PlayerComponent>(
+			g_networkEntityMap[
+				RootForce::Network::NetworkEntityID(
+					1,
+					RootForce::Network::ReservedActionID::CONNECT,
+					0
+				)
+			]
+		);
+		
+		EXPECT_EQ(secondPlayerComponent->Score, 1);
+		EXPECT_EQ(playerComponent->Deaths, 2);
+	}
+
+	world->GetEntityManager()->RemoveAllEntitiesAndComponents();
+	world->GetTagManager()->UnregisterAll();
+	world->GetGroupManager()->UnregisterAll();
+	g_engineContext.m_physics->RemoveAll();
+	delete world;
+}
