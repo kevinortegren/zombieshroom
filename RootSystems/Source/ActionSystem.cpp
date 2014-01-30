f0d4815789065b252888f9b560f6ddca3ffccbed
doqunbop <deadlyak@gmail.com>
2014-01-30 17:48:32 +0100

    Victory conditions, map reloading and score screen.

--- RootSystems/Source/ActionSystem.cpp
+++ RootSystems/Source/ActionSystem.cpp
@@ -4,6 +4,7 @@
 #include <RootSystems/Include/Network/NetworkComponents.h>
 #include <RootSystems/Include/Network/Messages.h>
 #include <RootEngine/Script/Include/RootScript.h>
+#include <RootSystems/Include/MatchStateSystem.h>
 
 extern RootEngine::GameSharedContext g_engineContext;
 extern RootForce::Network::NetworkEntityMap g_networkEntityMap;
@@ -38,60 +39,68 @@ namespace RootSystems
 		RootForce::HealthComponent* health = m_health.Get(p_entity);
 		RootForce::StateComponent* state = m_state.Get(p_entity);
 		RootForce::Animation* animation = m_animation.Get(p_entity);
-
-		if( health->IsDead )
-		{
-			if(action->ActivateAbility)
-			{
-				health->WantsRespawn = true;
-				action->ActivateAbility = false;
-				action->Jump = false;
-			}
-
-			return;
-		}
-
-		// Get the facing and calculate the right direction. Facing is assumed to be normalized, and up is assumed to be (0, 1, 0).
-		glm::vec3 facing = transform->m_orientation.GetFront();
-		glm::vec3 right = transform->m_orientation.GetRight();
-
-		// Calculate movement vector based on input values, the player's speed
-		glm::vec3 movement = facing * action->MovePower + right * action->StrafePower;
-		if(movement != glm::vec3(0))
-		{
-			movement = glm::normalize(movement) * playphys->MovementSpeed;
-			m_engineContext->m_physics->Move(*(collision->m_handle), movement + transform->m_position);
-		}
-
+		
 		// Rotate the model and reset the angle
 		transform->m_orientation.YawGlobal(action->Angle.x);
 		action->Angle.x = 0;
 
-		m_engineContext->m_physics->SetOrientation(*(collision->m_handle), transform->m_orientation.GetQuaternion());
-		
-		// Activate ability! Pew pew!
-		player->SelectedAbility = action->SelectedAbility - 1;
-		if(action->ActivateAbility && player->AbilityScripts[player->SelectedAbility].Cooldown <= 0)
+		bool isGameOver = false;
+		ECS::Entity* matchState = m_world->GetTagManager()->GetEntityByTag("MatchState");
+		if(matchState && m_world->GetEntityManager()->GetComponent<RootForce::TDMRuleSet>(matchState)->TimeLeft <= 0)
+			isGameOver = true;
+
+		if(!isGameOver)
 		{
-			std::string abilityScript = player->AbilityScripts[player->SelectedAbility].Name;
-			if (abilityScript != "")
+			if( health->IsDead )
 			{
-				m_engineContext->m_script->SetFunction(m_engineContext->m_resourceManager->GetScript(abilityScript), "OnCreate");
-				m_engineContext->m_script->AddParameterNumber(network->ID.UserID);
-				m_engineContext->m_script->AddParameterNumber(action->ActionID);
-				m_engineContext->m_script->ExecuteScript();
-				player->AbilityScripts[player->SelectedAbility].Charges --;
-				if(player->AbilityScripts[player->SelectedAbility].Charges == 0)
-					player->AbilityScripts[player->SelectedAbility] = RootForce::AbilityInfo();
+				if(action->ActivateAbility)
+				{
+					health->WantsRespawn = true;
+					action->ActivateAbility = false;
+					action->Jump = false;
+				}
+
+				return;
 			}
+
+			// Get the facing and calculate the right direction. Facing is assumed to be normalized, and up is assumed to be (0, 1, 0).
+			glm::vec3 facing = transform->m_orientation.GetFront();
+			glm::vec3 right = transform->m_orientation.GetRight();
+
+			// Calculate movement vector based on input values, the player's speed
+			glm::vec3 movement = facing * action->MovePower + right * action->StrafePower;
+			if(movement != glm::vec3(0))
+			{
+				movement = glm::normalize(movement) * playphys->MovementSpeed;
+				m_engineContext->m_physics->Move(*(collision->m_handle), movement + transform->m_position);
+			}
+
+			m_engineContext->m_physics->SetOrientation(*(collision->m_handle), transform->m_orientation.GetQuaternion());
+		
+			// Activate ability! Pew pew!
+			player->SelectedAbility = action->SelectedAbility - 1;
+			if(action->ActivateAbility && player->AbilityScripts[player->SelectedAbility].Cooldown <= 0)
+			{
+				std::string abilityScript = player->AbilityScripts[player->SelectedAbility].Name;
+				if (abilityScript != "")
+				{
+					m_engineContext->m_script->SetFunction(m_engineContext->m_resourceManager->GetScript(abilityScript), "OnCreate");
+					m_engineContext->m_script->AddParameterNumber(network->ID.UserID);
+					m_engineContext->m_script->AddParameterNumber(action->ActionID);
+					m_engineContext->m_script->ExecuteScript();
+					player->AbilityScripts[player->SelectedAbility].Charges --;
+					if(player->AbilityScripts[player->SelectedAbility].Charges == 0)
+						player->AbilityScripts[player->SelectedAbility] = RootForce::AbilityInfo();
+				}
+			}
+			if(player->AbilityScripts[0].Cooldown > 0)
+				player->AbilityScripts[0].Cooldown -= dt;
+			if(player->AbilityScripts[1].Cooldown > 0)
+				player->AbilityScripts[1].Cooldown -= dt;
+			if(player->AbilityScripts[2].Cooldown > 0)
+				player->AbilityScripts[2].Cooldown -= dt;
+			action->ActivateAbility = false;
 		}
-		if(player->AbilityScripts[0].Cooldown > 0)
-			player->AbilityScripts[0].Cooldown -= dt;
-		if(player->AbilityScripts[1].Cooldown > 0)
-			player->AbilityScripts[1].Cooldown -= dt;
-		if(player->AbilityScripts[2].Cooldown > 0)
-			player->AbilityScripts[2].Cooldown -= dt;
-		action->ActivateAbility = false;
 
 		if(state->CurrentState == RootForce::EntityState::ASCENDING)
 			animation->m_animClip = RootForce::AnimationClip::ASCEND;
@@ -107,17 +116,21 @@ namespace RootSystems
 		{
 			//if(action->StrafePower == 0 && action->MovePower == 0)
 			animation->m_animClip = RootForce::AnimationClip::IDLE;
-			if(action->MovePower < 0)
-				animation->m_animClip = RootForce::AnimationClip::WALKING;
-			else if(action->MovePower > 0)
-				animation->m_animClip = RootForce::AnimationClip::WALKING;
-			if(action->StrafePower > 0)
-				animation->m_animClip = RootForce::AnimationClip::STRAFE_RIGHT;
-			else if(action->StrafePower < 0)
-				animation->m_animClip = RootForce::AnimationClip::STRAFE_LEFT;
+			
+			if(!isGameOver)
+			{
+				if(action->MovePower < 0)
+					animation->m_animClip = RootForce::AnimationClip::WALKING;
+				else if(action->MovePower > 0)
+					animation->m_animClip = RootForce::AnimationClip::WALKING;
+				if(action->StrafePower > 0)
+					animation->m_animClip = RootForce::AnimationClip::STRAFE_RIGHT;
+				else if(action->StrafePower < 0)
+					animation->m_animClip = RootForce::AnimationClip::STRAFE_LEFT;
+			}
 		}
 		// Issue a jump if applicable
-		if(action->Jump)
+		if(!isGameOver && action->Jump)
 		{
 			m_engineContext->m_physics->PlayerJump(*(collision->m_handle), playphys->JumpForce);
 			if(animation->m_animClip != RootForce::AnimationClip::ASCEND && animation->m_animClip != RootForce::AnimationClip::DESCEND)
--- RootSystems/Source/MatchStateSystem.cpp
+++ RootSystems/Source/MatchStateSystem.cpp
@@ -33,12 +33,52 @@ namespace RootForce
 	{
 			return m_world->GetEntityManager()->GetComponent<TDMRuleSet>( m_world->GetTagManager()->GetEntityByTag("MatchState") )->TeamScore[p_team];
 	}
+	
+	struct Score
+	{
+		int Team;
+		std::string Name;
+		int Kills;
+		int Deaths;
+
+		Score(int p_team, std::string p_name, int p_kills, int p_deaths)
+			: Team(p_team), Name(p_name), Kills(p_kills), Deaths(p_deaths)
+		{}
+	};
+	bool CompareScores(Score &p_score1, Score &p_score2)
+	{
+		if(p_score1.Kills > p_score2.Kills) // If the score is higher
+			return true;
+		else if(p_score1.Kills == p_score2.Kills && p_score1.Deaths < p_score2.Deaths) // Or if equal, but less deaths
+			return true;
+		return false;
+	};
+	std::string MatchStateSystem::GetScoreList()
+	{
+		std::vector<Score> scoreList;
+		std::string scoreString = "[";
+		for(auto pair : g_networkEntityMap)
+		{
+			if(pair.first.ActionID != Network::ReservedActionID::CONNECT || pair.first.SequenceID != 0)
+				continue;
+
+			PlayerComponent* playerComponent = g_world->GetEntityManager()->GetComponent<PlayerComponent>(pair.second);
+			scoreList.push_back(Score(playerComponent->TeamID, playerComponent->Name, playerComponent->Score, playerComponent->Deaths));
+		}
+		std::sort(scoreList.begin(), scoreList.end(), &CompareScores);
+		for(auto score : scoreList)
+		{
+			scoreString += "["+std::to_string(score.Team)+",'" + score.Name + "',"+std::to_string(score.Kills)+","+std::to_string(score.Deaths)+"],";
+		}
+		scoreString += "]";
+		return scoreString;
+	}
 
 	bool MatchStateSystem::IsMatchOver()
 	{
 		ECS::Entity* matchState = m_world->GetTagManager()->GetEntityByTag("MatchState");
 		TDMRuleSet* ruleSet = m_world->GetEntityManager()->GetComponent<TDMRuleSet>( matchState );
-		if(ruleSet->ScoreLimit <= 0
+		if(ruleSet->ScoreLimit > 0
 			&& (ruleSet->TeamScore[1] >= ruleSet->ScoreLimit || 
 			ruleSet->TeamScore[2] >= ruleSet->ScoreLimit))
 			return true;
