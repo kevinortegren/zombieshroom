28ea8a3fea26edc8eca35c0fe463d1d9c30af817
doqunbop <deadlyak@gmail.com>
2014-02-04 10:11:00 +0100

    Fix the fix because the fix needed a fix to be fixed.

--- RootForce/Source/Ingamestate.cpp
+++ RootForce/Source/Ingamestate.cpp
@@ -214,7 +214,7 @@ namespace RootForce
 		g_engineContext.m_physics->RemoveAll();
 
 		// Disable the message handlers while resetting the server (to avoid null entities etc.)
-		if(!m_networkContext.m_server)
+		if(!m_networkContext.m_server.get())
 			m_networkContext.m_server->SetMessageHandler(nullptr);
 		m_networkContext.m_client->SetMessageHandler(nullptr);
 
