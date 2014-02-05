00982d73c20b66d2aff57263c0eafe0336fed7e9
Joel Begnert <joel.begnert@gmail.com>
2014-02-05 13:54:21 +0100

    updated script

--- Assets/Scripts/Explosion.lua
+++ Assets/Scripts/Explosion.lua
@@ -4,32 +4,24 @@ Explosion.pushback = 20;
 Explosion.cooldown = 0;
 
 function Explosion.OnCreate (userId, actionId)
-	--Logging.Log(LogLevel.DEBUG_PRINT, "0");
 	local self = Entity.New();
 	local playerEnt = Entity.GetEntityByNetworkID(userId, ReservedActionID.CONNECT, 0);
-	--Logging.Log(LogLevel.DEBUG_PRINT, "0");
-	local posVec = Entity.GetEntityByNetworkID(userId, ReservedActionID.CONNECT, 0):GetTransformation():GetPos();
-	--Logging.Log(LogLevel.DEBUG_PRINT, "1");
+	local posVec = Entity.GetEntityByNetworkID(userId, ReservedActionID.CONNECT, 0):GetCollisionResponder():GetCollisionPosition(playerEnt);
 	local frontVec = Entity.GetEntityByNetworkID(userId, ReservedActionID.CONNECT, 1):GetTransformation():GetOrient():GetFront();
 	local networkEnt = Network.New(self, userId, actionId);
-	--Logging.Log(LogLevel.DEBUG_PRINT, "1");
 
 	local collisionComp = Collision.New(self);
 	local colRespComp = CollisionResponder.New(self);
 	local physicsComp = Physics.New(self);
 	local transformComp = Transformation.New(self);
 	local scriptComp = Script.New(self, "Explosion");
-	--Logging.Log(LogLevel.DEBUG_PRINT, "2");
 	collisionComp:CreateHandle(self, 1, true);
 	transformComp:SetPos(posVec);
 	colRespComp:SetContainer(collisionComp);
 	physicsComp:CheckRadius(collisionComp:GetHandle(), Vec3.New(posVec.x, posVec.y, posVec.z), 20);
 	if Global.IsClient then
-		--Logging.Log(LogLevel.DEBUG_PRINT, "PARTICLE WOO!");
 		local particleComp = ParticleEmitter.New(self, "fireball");
 	end
-	--local playerComponent = playerEnt:GetPlayerComponent();
-	--playerComponent:StartCooldown(playerComponent:GetSelectedAbility(), Explosion.cooldown);
 end
 
 function Explosion.OnCollide (self, entity)
