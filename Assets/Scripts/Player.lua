1e44541ea3d805bfed0f8f6a4c2cca31ad505169
doqunbop <deadlyak@gmail.com>
2014-02-17 15:55:48 +0100

    Rename MagicMissile to AbilityRay and add an icon.

--- Assets/Scripts/Player.lua
+++ Assets/Scripts/Player.lua
@@ -38,7 +38,7 @@ function Player.OnCreate(userId, actionId)
 
 	playerComponent:SetAbility(0, "AbilityBall", -1);
 	playerComponent:SetAbility(1, "AbilityDash", -1);
-	playerComponent:SetAbility(2, "MagicMissile" ,-1);
+	playerComponent:SetAbility(2, "AbilityRay" ,-1);
 	playerComponent:SelectAbility(0);
 
 	playerPhysics:SetMovementSpeed(20);
