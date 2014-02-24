cdf9b1e0fb568f7dfe4cd4b4dcbf44e3bbf9c6f2
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2014-02-24 15:06:38 +0100

    Added destroy list to concurrent system

--- Utility/ECS/Source/EntitySystem.cpp
+++ Utility/ECS/Source/EntitySystem.cpp
@@ -66,6 +66,12 @@ void ECS::ConcurrentSystem::Process()
 	{
 		if(m_run)
 		{
+			for(auto itr = m_entitiesToRemove.begin(); itr != m_entitiesToRemove.end(); ++itr)
+			{
+				m_activeEntities.erase((*itr));
+			}
+			m_entitiesToRemove.clear();
+
 			Begin();
 
 			for(auto itr = m_activeEntities.begin(); itr != m_activeEntities.end(); ++itr)
