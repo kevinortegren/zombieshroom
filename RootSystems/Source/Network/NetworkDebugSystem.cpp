5ab25592f29e842e2ef9d2d25b0061301f08dc17
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2014-03-16 16:30:23 +0100

    Animation debug logged.

--- RootSystems/Source/Network/NetworkDebugSystem.cpp
+++ RootSystems/Source/Network/NetworkDebugSystem.cpp
@@ -15,6 +15,7 @@ namespace RootForce
 			SetUsage<NetworkComponent>();
 			SetUsage<PlayerComponent>();
 			SetUsage<Transform>();
+			SetUsage<Animation>();
 		}
 
 		void NetworkDebugSystem::Init()
@@ -22,6 +23,7 @@ namespace RootForce
 			m_networks.Init(m_world->GetEntityManager());
 			m_players.Init(m_world->GetEntityManager());
 			m_transforms.Init(m_world->GetEntityManager());
+			m_animations.Init(m_world->GetEntityManager());
 		}
 
 		void NetworkDebugSystem::ProcessEntity(ECS::Entity* p_entity)
@@ -30,10 +32,12 @@ namespace RootForce
 			NetworkComponent* network = m_networks.Get(p_entity);
 			PlayerComponent* player = m_players.Get(p_entity);
 			Transform* transform = m_transforms.Get(p_entity);
+			Animation* animation = m_animations.Get(p_entity);
 
 			std::stringstream ss;
 			ss << network->ID.UserID << ": " << player->Name << " (" << transform->m_position.x << ", " << transform->m_position.y << ", " << transform->m_position.z << ") ";
 			ss << "(" << player->AbilityScripts[0].Name << ", " << player->AbilityScripts[1].Name << ", " << player->AbilityScripts[2].Name << ") ";
+			ss << "(" << animation->LowerBodyAnim.m_animClip << ", " << animation->UpperBodyAnim.m_animClip << ") ";
 			ss << "<br/>";
 
 			g_engineContext.m_debugOverlay->AddHTMLToBuffer(ss.str().c_str());
