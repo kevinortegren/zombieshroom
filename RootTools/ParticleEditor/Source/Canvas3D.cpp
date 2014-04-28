57e256df8fecf3ffebcd8ab3d4ab6dc3d23e9106
Daniel Carlsson <dc.jarle@hotmail.com>
2014-04-29 00:38:24 +0200

    Adds sample code for Qt to push key events to SDL.

--- RootTools/ParticleEditor/Source/Canvas3D.cpp
+++ RootTools/ParticleEditor/Source/Canvas3D.cpp
@@ -10,6 +10,11 @@
 
 extern RootEngine::GameSharedContext g_engineContext;
 
+static void InitialiseKeymap();
+
+static const int KEYMAP_SIZE = 256;
+static SDL_Scancode s_keymap[KEYMAP_SIZE];
+
 Canvas3D::Canvas3D( QWidget* p_parent /*= 0*/ ) : QWidget(p_parent)
 {
 	//Flicker fix
@@ -52,6 +57,7 @@ Canvas3D::Canvas3D( QWidget* p_parent /*= 0*/ ) : QWidget(p_parent)
 	// Setup the SDL context
 	g_engineContext.m_renderer->SetupSDLContext(m_window.get());
 
+	InitialiseKeymap();
 }
 
 Canvas3D::~Canvas3D()
@@ -59,8 +65,77 @@ Canvas3D::~Canvas3D()
 
 }
 
+
+
 void Canvas3D::keyPressEvent( QKeyEvent *k )
 {
+	SDL_Event keyEvent;
+	keyEvent.type = SDL_KEYDOWN;
 
+	int key = k->key();
+
+	// 7-bit common ASCII can be sent right away.
+	if(key >= Qt::Key_Space && key <= Qt::Key_AsciiTilde) 
+	{		
+		keyEvent.key.keysym.scancode = SDL_GetScancodeFromKey(key);
+	} 
+	else // misc keys & modifiers needs to be identified.
+	{
+		keyEvent.key.keysym.scancode = s_keymap[key&0x00000011];
+	}
+
+	keyEvent.key.repeat = false;
+
+	SDL_PushEvent(&keyEvent);
 }
 
+static void InitialiseKeymap()
+{
+	// Initialize the key map with default values.
+	memset(s_keymap, 0, sizeof(s_keymap));
+
+	// Assign individual values.
+	s_keymap[Qt::Key_Alt&0x00000011] = SDL_SCANCODE_LALT;
+
+	/*Qt::Key_Escape	0x01000000	 
+		Qt::Key_Tab	0x01000001	 
+		Qt::Key_Backtab	0x01000002	 
+		Qt::Key_Backspace	0x01000003	 
+		Qt::Key_Return	0x01000004	 
+		Qt::Key_Enter	0x01000005	Typically located on the keypad.
+		Qt::Key_Insert	0x01000006	 
+		Qt::Key_Delete	0x01000007	 
+		Qt::Key_Pause	0x01000008	The Pause/Break key (Note: Not anything to do with pausing media)
+		Qt::Key_Print	0x01000009	 
+		Qt::Key_SysReq	0x0100000a	 
+		Qt::Key_Clear	0x0100000b	 
+		Qt::Key_Home	0x01000010	 
+		Qt::Key_End	0x01000011	 
+		Qt::Key_Left	0x01000012	 
+		Qt::Key_Up	0x01000013	 
+		Qt::Key_Right	0x01000014	 
+		Qt::Key_Down	0x01000015	 
+		Qt::Key_PageUp	0x01000016	 
+		Qt::Key_PageDown	0x01000017	 
+		Qt::Key_Shift	0x01000020	 
+		Qt::Key_Control	0x01000021	On Mac OS X, this corresponds to the Command keys.
+		Qt::Key_Meta	0x01000022	On Mac OS X, this corresponds to the Control keys. On Windows keyboards, this key is mapped to the Windows key.
+		Qt::Key_Alt	0x01000023	 
+		Qt::Key_AltGr	0x01001103	On Windows, when the KeyDown event for this key is sent, the Ctrl+Alt modifiers are also set.
+		Qt::Key_CapsLock	0x01000024	 
+		Qt::Key_NumLock	0x01000025	 
+		Qt::Key_ScrollLock	0x01000026	 
+		Qt::Key_F1	0x01000030	 
+		Qt::Key_F2	0x01000031	 
+		Qt::Key_F3	0x01000032	 
+		Qt::Key_F4	0x01000033	 
+		Qt::Key_F5	0x01000034	 
+		Qt::Key_F6	0x01000035	 
+		Qt::Key_F7	0x01000036	 
+		Qt::Key_F8	0x01000037	 
+		Qt::Key_F9	0x01000038	 
+		Qt::Key_F10	0x01000039	 
+		Qt::Key_F11	0x0100003a	 
+		Qt::Key_F12	0x0100003b */
+
+}
