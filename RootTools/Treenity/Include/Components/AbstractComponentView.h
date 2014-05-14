5ca6680b5fbcd7bd865669639125fc98babfcb5e
kevinortegren <kevinortegren@gmail.com>
2014-05-14 23:00:01 +0200

    New componentview style

--- RootTools/Treenity/Include/Components/AbstractComponentView.h
+++ RootTools/Treenity/Include/Components/AbstractComponentView.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <QWidget>
+#include <QFrame>
+#include <QPropertyAnimation>
 #include <RootTools/Treenity/Include/EngineInterface.h>
 #include <RootTools/Treenity/Include/EditorInterface.h>
 
-class AbstractComponentView : public QWidget
+class AbstractComponentView : public QFrame
 {
+	Q_OBJECT
 public:
 	AbstractComponentView(QWidget* p_parent = 0);
 	virtual ~AbstractComponentView();
@@ -15,7 +18,9 @@ public:
 
 	virtual const QString& GetComponentName() const = 0;
 	virtual void DisplayEntity(ECS::Entity* p_entity) = 0;
+
 protected:
 	EngineInterface* m_engineInterface;
 	EditorInterface* m_editorInterface;
+
 };
--- RootTools/Treenity/Source/ComponentViewItem.cpp
+++ RootTools/Treenity/Source/ComponentViewItem.cpp
@@ -2,16 +2,15 @@
 
 ComponentViewItem::ComponentViewItem(AbstractComponentView* p_item, QWidget* p_parent /*= 0*/ ) : QWidget(p_parent), m_item(p_item)
 {
-	m_label = new QLabel(p_item->GetComponentName(), this);
-	m_label->setStyleSheet("background-color: #D9D9D9;color: #000000;");
+	m_headerButton = new QPushButton(p_item->GetComponentName(), this);
 	m_layout = new QVBoxLayout(this);
 	m_layout->setContentsMargins(0, 0, 0, 0);
-	m_layout->addWidget(m_label);
+	m_layout->addWidget(m_headerButton);
 	m_layout->addWidget(p_item);
 	m_layout->setSizeConstraint(QLayout::SetNoConstraint);
 	setLayout(m_layout);
 
-	//p_item->setVisible(false);
+	connect(m_headerButton, SIGNAL(clicked()), this, SLOT(HeaderClicked()));
 }
 
 ComponentViewItem::~ComponentViewItem()
@@ -22,20 +21,17 @@ ComponentViewItem::~ComponentViewItem()
 	}
 
 	m_item->setParent(nullptr);
-	delete m_label;
+	delete m_headerButton;
 	delete m_layout;
-}
-
-void ComponentViewItem::mousePressEvent( QMouseEvent* event )
-{
-	if(m_label->geometry().contains(event->pos()))
-	{
-		m_item->setVisible(!m_item->isVisible());
-	}
 }
 
 AbstractComponentView* ComponentViewItem::GetItem()
 {
 	return m_item;
+}
+
+void ComponentViewItem::HeaderClicked()
+{
+	m_item->setVisible(!m_item->isVisible());
 }
 
