648df21bf002ccad0a1a72e879a9eef0340c51c2
kevinortegren <kevinortegren@gmail.com>
2014-05-27 14:35:31 +0200

    New lua libs

--- CMake/Modules/FindLUA.cmake
+++ CMake/Modules/FindLUA.cmake
@@ -5,8 +5,8 @@ find_path(LUA_INCLUDE_DIRS
     HINTS "${CMAKE_SOURCE_DIR}/External/Include")
 
 find_library(LUA_LIBRARY_DEBUG
-    NAMES lua
+    NAMES lua52
     HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Debug")
 find_library(LUA_LIBRARY
-    NAMES lua
+    NAMES lua52
     HINTS "${CMAKE_SOURCE_DIR}/External/Lib/x86/Release")
